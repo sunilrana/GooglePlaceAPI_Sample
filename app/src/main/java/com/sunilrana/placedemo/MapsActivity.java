a9cc46462e92e50f14ba092233799033fcc09309
sunil <sunilrana855@gmail.com>
2019-05-02 16:21:22 -0400

    Initial commit

---
+++ app/src/main/java/com/sunilrana/placedemo/MapsActivity.java
@@ -0,0 +1,55 @@
+package com.sunilrana.placedemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+
+public class MapsActivity extends AppCompatActivity implements PlacesAutoCompleteAdapter.ClickListener{
+
+    private PlacesAutoCompleteAdapter mAutoCompleteAdapter;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_act);
+
+        Places.initialize(this, getResources().getString(R.string.google_maps_key));
+
+        recyclerView = (RecyclerView) findViewById(R.id.places_recycler_view);
+        ((EditText) findViewById(R.id.place_search)).addTextChangedListener(filterTextWatcher);
+
+        mAutoCompleteAdapter = new PlacesAutoCompleteAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAutoCompleteAdapter.setClickListener(this);
+        recyclerView.setAdapter(mAutoCompleteAdapter);
+        mAutoCompleteAdapter.notifyDataSetChanged();
+    }
+
+    private TextWatcher filterTextWatcher = new TextWatcher() {
+        public void afterTextChanged(Editable s) {
+            if (!s.toString().equals("")) {
+                mAutoCompleteAdapter.getFilter().filter(s.toString());
+                if (recyclerView.getVisibility() == View.GONE) {recyclerView.setVisibility(View.VISIBLE);}
+            } else {
+                if (recyclerView.getVisibility() == View.VISIBLE) {recyclerView.setVisibility(View.GONE);}
+            }
+        }
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+        public void onTextChanged(CharSequence s, int start, int before, int count) { }
+    };
+
+    @Override
+    public void click(Place place) {
+        Toast.makeText(this, place.getAddress()+", "+place.getLatLng().latitude+place.getLatLng().longitude, Toast.LENGTH_SHORT).show();
+    }
+}
